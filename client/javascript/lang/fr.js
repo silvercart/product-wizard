5eb03a2d442648cff996a006123d53e6b808c5d3
Sebastian Diel <sdiel@pixeltricks.de>
2020-08-21 14:38:00 +0200

    ENHANCEMENT: Updated i18n.

---
+++ client/javascript/lang/fr.js
@@ -0,0 +1,11 @@
+if (typeof (ss) === 'undefined' || typeof (ss.i18n) === 'undefined') {
+    if (typeof (console) !== 'undefined') {
+        console.error('Class ss.i18n not defined');
+    }
+} else {
+    ss.i18n.addDictionary('fr', {
+        "SilverCart.ProductWizard.ERROR.PickOptionHeading": "Une erreur s'est produite",
+        "SilverCart.ProductWizard.ERROR.PickOptionContent": "Sélectionnez au moins une option avant de continuer.",
+        "SilverCart.ProductWizard.ERROR.PickOptions": "Sélectionnez une option pour chaque offre."
+    });
+}
---
+++ client/javascript/lang/nl.js
@@ -0,0 +1,10 @@
+if (typeof (ss) == 'undefined' || typeof (ss.i18n) == 'undefined') {
+    if (typeof (console) != 'undefined')
+        console.error('Class ss.i18n not defined');
+} else {
+    ss.i18n.addDictionary('de', {
+        "SilverCart.ProductWizard.ERROR.PickOptionHeading": "Er heeft zich een fout voorgedaan",
+        "SilverCart.ProductWizard.ERROR.PickOptionContent": "Selecteer ten minste een optie voordat u verdergaat.",
+        "SilverCart.ProductWizard.ERROR.PickOptions": "Selecteer een optie voor elke aanbieding."
+    });
+}
