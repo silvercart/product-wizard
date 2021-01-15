f9dbf8c155fe350017d9667abb340d67a57b6b3a
Sebastian Diel <sdiel@pixeltricks.de>
2021-01-15 14:47:23 +0100

    MINOR-BUGFIX: Fixed i18n issue (wrong dictionary key in nl.js).

--- client/javascript/lang/nl.js
+++ client/javascript/lang/nl.js
@@ -1,8 +1,9 @@
-if (typeof (ss) == 'undefined' || typeof (ss.i18n) == 'undefined') {
-    if (typeof (console) != 'undefined')
+if (typeof (ss) === 'undefined' || typeof (ss.i18n) === 'undefined') {
+    if (typeof (console) !== 'undefined') {
         console.error('Class ss.i18n not defined');
+    }
 } else {
-    ss.i18n.addDictionary('de', {
+    ss.i18n.addDictionary('nl', {
         "SilverCart.ProductWizard.ERROR.PickOptionHeading": "Er heeft zich een fout voorgedaan",
         "SilverCart.ProductWizard.ERROR.PickOptionContent": "Selecteer ten minste een optie voordat u verdergaat.",
         "SilverCart.ProductWizard.ERROR.PickOptions": "Selecteer een optie voor elke aanbieding."
