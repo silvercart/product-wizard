7d832300662b2bbe0649a06842270c4e0496f6b0
Sebastian Diel <sdiel@pixeltricks.de>
2021-05-25 16:49:51 +0200

    MINOR: Updated JS i18n.

--- client/javascript/lang/de.js
+++ client/javascript/lang/de.js
@@ -3,9 +3,10 @@ if (typeof (ss) == 'undefined' || typeof (ss.i18n) == 'undefined') {
         console.error('Class ss.i18n not defined');
 } else {
     ss.i18n.addDictionary('de', {
+        "SilverCart.ProductWizard.ERROR.PickOneProductOption": "Bitte wählen Sie mindestens ein Produkt.",
         "SilverCart.ProductWizard.ERROR.PickOptionHeading": "Es ist ein Fehler aufgetreten",
         "SilverCart.ProductWizard.ERROR.PickOptionContent": "Bitte wählen Sie mindestens eine Option, um fortzufahren.",
         "SilverCart.ProductWizard.ERROR.PickOptions": "Bitte wählen Sie zu jedem Angebot eine Option aus.",
-        "SilverCart.ProductWizard.ERROR.NoProductsYet": "Sie haben noch keine Artikel ausgewählt."
+        "SilverCart.ProductWizard.ERROR.NoProductsYet": "Sie haben noch keine Artikel ausgewählt.",
     });
 }
--- client/javascript/lang/en.js
+++ client/javascript/lang/en.js
@@ -3,9 +3,10 @@ if (typeof (ss) == 'undefined' || typeof (ss.i18n) == 'undefined') {
         console.error('Class ss.i18n not defined');
 } else {
     ss.i18n.addDictionary('en', {
+        "SilverCart.ProductWizard.ERROR.PickOneProductOption": "Please choose at least one product.",
         "SilverCart.ProductWizard.ERROR.PickOptionHeading": "An error has occurred",
         "SilverCart.ProductWizard.ERROR.PickOptionContent": "Please select at least one option to continue.",
         "SilverCart.ProductWizard.ERROR.PickOptions": "Please select one option for each offer.",
-        "SilverCart.ProductWizard.ERROR.NoProductsYet": "You have not selected any items yet."
+        "SilverCart.ProductWizard.ERROR.NoProductsYet": "You have not selected any items yet.",
     });
 }
